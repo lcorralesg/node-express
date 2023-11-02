480bed1eac4596c9258c26f71757d0829bfbf59c
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-11-01 23:15:54 -0500

    mongo

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose"
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/login_google',{
+        await mongoose.connect('mongodb://mongodb:27017/login_google',{
         }).then(db => console.log('Database is connected'));
     } catch (error) {
         console.log(error);
