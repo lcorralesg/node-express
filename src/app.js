daac0b7f7ffafa4a0353e264f6a2603e9a113c9c
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-11-04 23:55:08 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000",
+    "http://54.166.217.206:5173", "http://54.166.217.206:3000"],
     credentials: true
 }));
 app.use(express.json());
