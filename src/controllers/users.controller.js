75c05907b7e4a6781316a030832639dfc9e73ab0
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-11-05 01:10:11 -0500

    status

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -6,7 +6,13 @@ export const getUsers = async (req, res) => {
     .status(200);
 }
 export const deleteUser = async (req, res) => {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if(!user) return res.status(404).json({message: "User not found"});
-    res.status(204);
-}
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.sendStatus(204);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
